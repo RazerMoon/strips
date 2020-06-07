c692f602af6d7970d02d9a2b75bbc1d72c1ed7c6
RazerMoon <gamespeterplelit@gmail.com>
2020-06-07 19:44:47 +0100

    Update auth.example.php

--- scripts/config/auth.example.php
+++ scripts/config/auth.example.php
@@ -8,7 +8,7 @@
     define('AUTH_CLIENT_SECRET', '');                                               // Client secret.
     define('AUTH_GRANT_TYPE', '');                                                  // Grant type.
     define('AUTH_REDIRECT_URL', 'http://localhost/scripts/logreg.php');             // The URL that ouath redirects to - DONT CHANGE THIS.
-    define('AUTH_SCOPE', 'identify email');                                         // Scopes - YOU AT LEAST NEED IDENTIFY.
+    define('AUTH_SCOPE', 'identify');                                               // Scopes - YOU AT LEAST NEED IDENTIFY.
     define('AUTH_TOKEN_URL', 'https://discordapp.com/api/oauth2/token');            // Token URL - DONT CHANGE THIS.
     define('AUTH_EXCHANGE_URL', 'https://discordapp.com/api/v7/users/@me');         // Token exchange URL - DONT CHANGE THIS.
     define('AUTH_URL', 'Location: https://discordapp.com/api/oauth2/authorize?');   // Authentication URL.
